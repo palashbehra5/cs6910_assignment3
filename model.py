c8751c82b79724fd18434fae6ab494237802cf9c
lord of the frenzied flame <40783095+palashbehra5@users.noreply.github.com>
2023-05-12 14:37:01 +0530

    ATTENTION!

--- model.py
+++ model.py
@@ -181,12 +181,12 @@ def compare_sequences(batch1, batch2):
 
     return num_same_sequences
 
-def test_model_instance(configs):
+def test_model_instance(configs, mode, batch_size):
 
     count = 0
 
     VOCAB_SIZE = 131
-    BATCH_SIZE = 4
+    BATCH_SIZE = batch_size
     MAX_SEQ_SIZE = 28
 
     source = torch.randint(low=0, high=VOCAB_SIZE, size=(BATCH_SIZE, MAX_SEQ_SIZE)).to(device)
@@ -199,7 +199,15 @@ def test_model_instance(configs):
         input_embedding_size, num_encoder_layers, num_decoder_layers, hidden_layer_size, cell_type_encoder, cell_type_decoder, bidirectional, dropout, teacher_forcing = config
     
         # Create an instance of the seq2seq model using the parameter values
-        model = seq2seq(VOCAB_SIZE, input_embedding_size, hidden_layer_size, num_encoder_layers, num_decoder_layers,
+        if(mode=='attn'):
+
+            model = seq2seq_attn(VOCAB_SIZE, input_embedding_size, hidden_layer_size, num_encoder_layers, num_decoder_layers,
+                   dropout, bidirectional, cell_type_encoder, cell_type_decoder, teacher_forcing,
+                   BATCH_SIZE, MAX_SEQ_SIZE, debugging=False).to(device)
+
+        else:
+
+            model = seq2seq(VOCAB_SIZE, input_embedding_size, hidden_layer_size, num_encoder_layers, num_decoder_layers,
                    dropout, bidirectional, cell_type_encoder, cell_type_decoder, teacher_forcing,
                    BATCH_SIZE, MAX_SEQ_SIZE, debugging=False).to(device)
         
@@ -209,17 +217,13 @@ def test_model_instance(configs):
         
     print("PASSED {} CONFIGS.".format(count))
 
-import torch
-import torch.nn as nn
-import random
-
-class seq2seq(nn.Module):
+class seq2seq_attn(nn.Module):
 
     def __init__(self, vocab_size, embedding_dim, hidden_dim, num_layers_encoder, num_layers_decoder, 
                  dropout, bidirectional, encoder_cell_type, decoder_cell_type, teacher_forcing, 
                  batch_size, max_seq_size, debugging=False):
 
-        super(seq2seq, self).__init__()
+        super(seq2seq_attn, self).__init__()
 
         self.output_size = vocab_size
         self.embedding_dim = embedding_dim
@@ -243,10 +247,9 @@ class seq2seq(nn.Module):
         self.rnn_decoder = self.cell(num_layers_decoder, decoder_cell_type, 0)
 
         # Attention layer
-        self.attention = nn.Linear(hidden_dim, hidden_dim)
-
-        # Final layer for calculating probabilities
-        self.fc1 = nn.Linear(self.hidden_dim, self.output_size)
+        num_directions_encoder = 1 if bidirectional == 0 else 2
+        self.attention = nn.Linear(hidden_dim, hidden_dim)  # Attention layer
+        self.fc1 = nn.Linear(self.hidden_dim * 2, self.output_size)  # Output layer
 
     def cell(self, num_layers, cell_type, bidirectional):
 
@@ -292,6 +295,7 @@ class seq2seq(nn.Module):
         return decoder_state, decoder_cell
 
     def forward(self, x, y):
+
         x.to(device)
         y.to(device)
 
@@ -303,6 +307,7 @@ class seq2seq(nn.Module):
         num_directions = 2 if self.bidirectional else 1
         SOS_TOKEN = 128
 
+        # Calculate embedding first
         x = self.embedding_encoder(x)
 
         if self.encoder_cell_type == "LSTM":
@@ -311,18 +316,31 @@ class seq2seq(nn.Module):
             encoder_output, encoder_hidden = self.rnn_encoder(x)
             encoder_cell = None
 
-        decoder_state, decoder_cell = self.initialize_decoder_state(self.encoder_cell_type, encoder_hidden, encoder_cell, 
-                                                                     self.decoder_cell_type, self.bidirectional, 
-                                                                     num_layers_decoder, num_layers_encoder)
+        # Modify the encoder_hidden based on bidirectional flag
+        if self.bidirectional:
+
+            forward_output = encoder_output[:, :, :self.hidden_dim]
+            backward_output = encoder_output[:, :, self.hidden_dim:]
+            encoder_output = forward_output + backward_output
+
+        decoder_state, decoder_cell = self.initialize_decoder_state(
+            self.encoder_cell_type, encoder_hidden, encoder_cell, self.decoder_cell_type,
+            self.bidirectional, num_layers_decoder, num_layers_encoder
+        )
 
         decoder_inputs = torch.full((batch_size, 1), SOS_TOKEN).to(device)
         decoder_outputs = torch.empty((self.max_seq_size, batch_size, self.output_size)).to(device)
 
         for t in range(self.max_seq_size):
+
+            
+
             decoder_inputs = self.embedding_decoder(decoder_inputs.to(device))
 
             if self.decoder_cell_type == "LSTM":
-                decoder_output, (decoder_state, decoder_cell) = self.rnn_decoder(decoder_inputs, (decoder_state.contiguous(), decoder_cell.contiguous()))
+                decoder_output, (decoder_state, decoder_cell) = self.rnn_decoder(
+                    decoder_inputs, (decoder_state.contiguous(), decoder_cell.contiguous())
+                )
             else:
                 decoder_output, decoder_state = self.rnn_decoder(decoder_inputs, decoder_state.contiguous())
                 decoder_cell = None
@@ -330,29 +348,128 @@ class seq2seq(nn.Module):
             if num_layers_decoder > 1:
                 decoder_output = self.dropout(decoder_output)
 
-            # Attention mechanism
-            attn_weights = torch.softmax(self.attention(encoder_output), dim=1)
-            context = torch.bmm(attn_weights.unsqueeze(1), encoder_output).squeeze(1)
+            attn_weights = torch.matmul(self.attention(encoder_output), decoder_output.transpose(1, 2))
 
-            decoder_output = torch.cat((decoder_output, context), dim=1)
+            attn_weights = attn_weights.squeeze(dim=2)
 
+            attn_weights = torch.softmax(attn_weights, dim=1)
+
+            context_vector = torch.bmm(attn_weights.unsqueeze(1), encoder_output).squeeze(dim=1)
+
+            decoder_output = decoder_output.squeeze(dim=1)
+
+            decoder_output = torch.cat((decoder_output, context_vector), dim=1)
             decoder_outputs[t] = self.fc1(decoder_output).squeeze(dim=1)
 
-
-            
-
+            # Determine whether to use teacher forcing or predicted output
             use_teacher_forcing = random.random() < self.teacher_forcing_prob
 
+            # Obtain the next input to the decoder
             if use_teacher_forcing:
-                decoder_inputs = y[:, t].unsqueeze(0)
+                decoder_inputs = y[:, t].unsqueeze(0)  # Use ground truth input
             else:
-                indices = torch.argmax(decoder_outputs[t], dim=1)
+                indices = torch.argmax(decoder_output, dim=1)
                 decoder_inputs = indices.unsqueeze(dim=1)
 
             if decoder_inputs.shape[0] != batch_size:
                 decoder_inputs = decoder_inputs.transpose(0, 1)
 
         decoder_outputs = decoder_outputs.transpose(0, 1)
-
         return decoder_outputs
+    
+# This does not seem to work
+# Go for standard implementation
+##################################################################################   
+# https://pytorchnlp.readthedocs.io/en/latest/_modules/torchnlp/nn/attention.html#
+##################################################################################
 
+class Attention(nn.Module):
+
+    """ Applies attention mechanism on the `context` using the `query`.
+
+    **Thank you** to IBM for their initial implementation of :class:`Attention`. Here is
+    their `License
+    <https://github.com/IBM/pytorch-seq2seq/blob/master/LICENSE>`__.
+
+    Args:
+        dimensions (int): Dimensionality of the query and context.
+        attention_type (str, optional): How to compute the attention score:
+
+            * dot: :math:`score(H_j,q) = H_j^T q`
+            * general: :math:`score(H_j, q) = H_j^T W_a q`
+
+    Example:
+
+         >>> attention = Attention(256)
+         >>> query = torch.randn(5, 1, 256)
+         >>> context = torch.randn(5, 5, 256)
+         >>> output, weights = attention(query, context)
+         >>> output.size()
+         torch.Size([5, 1, 256])
+         >>> weights.size()
+         torch.Size([5, 1, 5])
+    """
+
+    def __init__(self, dimensions, attention_type='general'):
+        super(Attention, self).__init__()
+
+        if attention_type not in ['dot', 'general']:
+            raise ValueError('Invalid attention type selected.')
+
+        self.attention_type = attention_type
+        if self.attention_type == 'general':
+            self.linear_in = nn.Linear(dimensions, dimensions, bias=False)
+
+        self.linear_out = nn.Linear(dimensions * 2, dimensions, bias=False)
+        self.softmax = nn.Softmax(dim=-1)
+        self.tanh = nn.Tanh()
+
+
+    def forward(self, query, context):
+        """
+        Args:
+            query (:class:`torch.FloatTensor` [batch size, output length, dimensions]): Sequence of
+                queries to query the context.
+            context (:class:`torch.FloatTensor` [batch size, query length, dimensions]): Data
+                overwhich to apply the attention mechanism.
+
+        Returns:
+            :class:`tuple` with `output` and `weights`:
+            * **output** (:class:`torch.LongTensor` [batch size, output length, dimensions]):
+              Tensor containing the attended features.
+            * **weights** (:class:`torch.FloatTensor` [batch size, output length, query length]):
+              Tensor containing attention weights.
+        """
+        batch_size, output_len, dimensions = query.size()
+        query_len = context.size(1)
+
+        if self.attention_type == "general":
+            query = query.reshape(batch_size * output_len, dimensions)
+            query = self.linear_in(query)
+            query = query.reshape(batch_size, output_len, dimensions)
+
+        # TODO: Include mask on PADDING_INDEX?
+
+        # (batch_size, output_len, dimensions) * (batch_size, query_len, dimensions) ->
+        # (batch_size, output_len, query_len)
+        attention_scores = torch.bmm(query, context.transpose(1, 2).contiguous())
+
+        # Compute weights across every context sequence
+        attention_scores = attention_scores.view(batch_size * output_len, query_len)
+        attention_weights = self.softmax(attention_scores)
+        attention_weights = attention_weights.view(batch_size, output_len, query_len)
+
+        # (batch_size, output_len, query_len) * (batch_size, query_len, dimensions) ->
+        # (batch_size, output_len, dimensions)
+        mix = torch.bmm(attention_weights, context)
+
+        # concat -> (batch_size * output_len, 2*dimensions)
+        combined = torch.cat((mix, query), dim=2)
+        combined = combined.view(batch_size * output_len, 2 * dimensions)
+
+        # Apply linear_out on every 2nd dimension of concat
+        # output -> (batch_size, output_len, dimensions)
+        output = self.linear_out(combined).view(batch_size, output_len, dimensions)
+        output = self.tanh(output)
+
+        return output, attention_weights
